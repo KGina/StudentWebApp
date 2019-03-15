376fcd3fdea6c59332bb4a882417821e1ec7ce37
Mcuser <Mcuser@ditinfo.ac.za>
2019-03-15 09:36:48 +0200

    Add project files.

---
+++ StudentWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentWebApp.Startup))]
+namespace StudentWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
